6d631421c4e335682d8485996c78b560389d62b3
Megan Dacus <meganjustice0609@gmail.com>
2023-05-09 15:18:03 +1000

    Assignment Completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        word_letters = @word.chars
+        
+        array.filter do |word|
+            array_letters = word.chars
+            
+            if word_letters.sort == array_letters.sort
+                 word
+            else
+                nil
+            end 
+        end 
+    end
+end
+
+
+cat = Anagram.new('cat')
+puts cat.match(%w[pat, tar, life, love])
